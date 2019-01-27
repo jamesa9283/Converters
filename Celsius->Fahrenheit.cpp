475f95efc935675b50587bc79506be24115678d1
James Arthur <jamesarthur7@icloud.com>
2019-01-27 16:12:19 +0000

    Create Celsius->Fahrenheit.cpp

---
+++ Celsius->Fahrenheit.cpp
@@ -0,0 +1,24 @@
+//
+// Conversion - Program to convert temperature from
+//              Celsius degrees into Fahrenheit :
+//              Fahrenheit = Celsius * (212 - 32)/100 + 32
+//
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+fahrenheit = 0
+celcius = 0
+
+int main(int nNumberofArgs, char* pszArgs[])
+{
+   // enter the temperature in Celsius
+   int Celsius;
+   cout << "Enter the Degrees in Celsius"
+   cin >> celsius;
+
+   // convert Celsius into Fahrenheit values
+   fahrenheit = celsius * (212 - 32)/100 + 32
+   cout << fahrenheit
+}
